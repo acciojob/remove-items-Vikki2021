5283f94c5ba03083c14180f702cbfa59ab8f19e7
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-03-07 23:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+let arr=['Red','Green','White','Black']
+
+function deleteItem()
+{
+	let val=document.getElementById('colorSelect').value;
+	let sela=document.getElementById('colorSelect');
+	
+	sela.innerHTML="";
+	arr = arr.filter(color => color !== val);
+	
+	arr.forEach((item)=>{
+			sela.innerHTML+=`
+				<option>${item}</option> 
+			`
+	})
+}
